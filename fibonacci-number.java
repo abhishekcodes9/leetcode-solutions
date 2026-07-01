dadd29b448fbaf0a87ac86927a43c13bd7ea5c5d
Abhishek Agarwal <abhishekagarwalofficial9@gmail.com>
2026-07-01 22:32:08 +0530

    Implement Fibonacci number calculation

---
+++ fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        int first=0;
+        int second=1; 
+
+        for(int i=0;i<n;i++){
+            int sum = first+second;
+            first=second;
+            second=sum;
+        }
+        return first;
+    }
+}
